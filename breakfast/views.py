d7816b55ca74b74f4b56c798f9a9bf759758ee09
bob <2106397m@student.gla.ac.uk>
2019-03-05 21:33:07 +0000

    changes to continent and recipe views

--- breakfast/views.py
+++ breakfast/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
-from django.http import HttpResponse
 from breakfast.models import Continent
+from breakfast.models import Recipe
 
 
 # Create your views here.
@@ -23,15 +23,42 @@ def sign_up(request):
 def my_account(request):
     return render(request, 'breakfast/my_account.html', {})
 
-def continent_page(request, Continent_name_slug):
-    context_dict {}
+def continent_page(request, continent_name_slug):
+    
+    context_dict = {}
 
     try:
-        continent = Continent.objects.get[slug=Continent_name_slug]
+        continent = Continent.objects.get(slug=continent_name_slug)
+        recipe = Recipe.objects.filter(continent=continent)
 
-    return render(request, 'breakfast/continent_page.html', {})
+        context_dict['recipe'] = recipe
+        context_dict['continent'] = continent
+    except Continent.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['recipe'] = None
 
-def recipe_page(request):
-    return render(request, 'breakfast/recipe_page.html')
+    return render(request, 'breakfast/continent_page.html', context_dict)
+
+def recipe_page(request, recipe_name_slug):
+
+    context_dict = {}
+
+    try:
+        recipe = Recipe.object.get(slug=recipe_name_slug)
+        continent = Recipe.object.get(continent)
+        description = Recipe.object.get(description)
+        ingredients = Recipe.object.get(ingredients)
+
+        context_dict['recipe'] = recipe
+        context_dict['continent'] = continent
+        context_dict['description'] = description
+        context_dict['ingredients'] = ingredients
+    except Recipe.DoesNotExist:
+        context_dict['recipe'] = None
+        context_dict['continent'] = None
+        context_dict['description'] = None
+        context_dict['ingredients'] = None
+        
+    return render(request, 'breakfast/recipe_page.html', context_dict)
    
 
