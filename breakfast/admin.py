d0a19a8e92c58bbf5293285b258cc6b07e073219
Irene Zhang <46045277+susancat@users.noreply.github.com>
2019-03-12 01:20:16 +0000

    register and login work

--- breakfast/admin.py
+++ breakfast/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from breakfast.models import Continent, Recipe, Review, Favourites
+from breakfast.models import Continent, Recipe, Review, Favourites, UserProfile
 
 class ContinentAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Continent, ContinentAdmin)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = {'slug': ('recipe_name',)}
+
+admin.site.register(Continent,ContinentAdmin)
 admin.site.register(Recipe)
 admin.site.register(Review)
 admin.site.register(Favourites)
+admin.site.register(UserProfile)
