894a16fb64c02f5d6935e3f5b4e8a1344d833084
bmoyler <2106397m@student.gla.ac.uk>
2019-03-19 13:34:25 +0000

    gone wrong somewhere

--- breakfast/search_indexes.py
+++ breakfast/search_indexes.py
@@ -3,13 +3,12 @@ from breakfast.models import Recipe
 
 class RecipeIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    recipe_name = indexes.CharField(model_attr='recipe_name', faceted=True)
-    continent = indexes.CharField(model_attr='continent', faceted=True)
-    labels = indexes.CharField(model_attr='labels', faceted=True)
+    recipe_name = indexes.CharField(model_attr='recipe_name')
+    continent = indexes.CharField(model_attr='continent')
+    labels = indexes.CharField(model_attr='labels')
     ingredients = indexes.CharField(model_attr='ingredients')
     short_description = indexes.CharField(model_attr='short_description')
-    
-    
+
     rendered = indexes.CharField(use_template=True, indexed=False)
 
     def get_model(self):
